49b75d87a7ac3ef23b2f48a6b85c4de25a38485b
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-10-13 08:25:11 -0500

    feat: Add base components, navbar, shadcn, next-theme and responsive desing

---
+++ src/app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <section className="h-[calc(100vh-7rem)] flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-slate-200 mb-4">Home</h1>
+    </section>
+  );
+}
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Poppins } from "next/font/google";
+
+export const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export const inter = Inter({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
