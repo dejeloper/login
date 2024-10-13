bca2d0addb18177ba6e7af7f36137d707cf96a6a
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-10-13 17:30:02 -0500

    style: Update title in dark/light mode

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,7 +4,9 @@ import { signOut } from "next-auth/react";
 export default function DashboardPage() {
   return (
     <section className="h-[calc(100vh-7rem)] flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-slate-200 mb-4">Dashboard</h1>
+      <h1 className="flex items-center mb-6 text-4xl font-semibold text-gray-900 dark:text-white">
+        Dashboard
+      </h1>
       <button
         className="bg-blue-500 text-white px-12 py-2 rounded-lg mt-4"
         onClick={() => signOut()}
--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,7 +1,9 @@
 export default function SettingsPage() {
   return (
     <section className="h-[calc(100vh-7rem)] flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-slate-200 mb-4">Settings</h1>
+      <h1 className="flex items-center mb-6 text-4xl font-semibold text-gray-900 dark:text-white">
+        Settings
+      </h1>
     </section>
   );
 }
